a53bf297f80443a8286b432dc54c7bb62f358286
Shigeo Hakkaku <shigeo.hakkaku@yukawa.kyoto-u.ac.jp>
2020-06-22 00:38:34 +0900

    classical preprocessingをしないとだめなんかな。

---
+++ 2020summer/host.py
@@ -0,0 +1,40 @@
+import json
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+plt.style.use('ggplot')
+
+import qsharp
+qsharp.packages.add("Microsoft.Quantum.MachineLearning::0.11.2006.403")
+qsharp.reload()
+
+from Microsoft.Quantum.Samples import (
+    TrainHalfMoonModel, ValidateHalfMoonModel, ClassifyHalfMoonModel
+)
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    init = float(sys.argv[2])
+    with open(filename) as f:
+        data = json.load(f)
+    parameter_starting_points = [
+            [init],
+     ]
+
+    (parameters, bias) = TrainHalfMoonModel.simulate(
+        trainingVectors=data['Features'],
+        trainingLabels=data['Labels'],
+        initialParameters=parameter_starting_points
+    )
+    print((parameters, bias))
+
+    miss_rate = ValidateHalfMoonModel.simulate(
+        validationVectors=data['Features'],
+        validationLabels=data['Labels'],
+        parameters=parameters, bias=bias
+    )
+
+    print(f"Miss rate: {miss_rate:0.2%}")
